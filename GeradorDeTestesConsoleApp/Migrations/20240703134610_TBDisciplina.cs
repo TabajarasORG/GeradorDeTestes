f503a23dcb8de6aca844b926071a5361321487b6
Leonardo Dallago de Cordova <leodallago@outlook.com>
2024-07-03 10:57:45 -0300

    implementacao Entity Framework

---
+++ GeradorDeTestesConsoleApp/Migrations/20240703134610_TBDisciplina.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeradorDeTestesConsoleApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class TBDisciplina : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TBDisciplina",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "varchar(150)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBDisciplina", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Materia",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DisciplinaId = table.Column<int>(type: "int", nullable: false),
+                    Serie = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Materia", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Materia_TBDisciplina_DisciplinaId",
+                        column: x => x.DisciplinaId,
+                        principalTable: "TBDisciplina",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Materia_DisciplinaId",
+                table: "Materia",
+                column: "DisciplinaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Materia");
+
+            migrationBuilder.DropTable(
+                name: "TBDisciplina");
+        }
+    }
+}
---
+++ GeradorDeTestesConsoleApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using GeradorDeTestes.ModuloDisciplina;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeradorDeTestesConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            GeradorTesteDbContext dbContext = new();
+
+            var disciplina = new Disciplina { Nome = "test"};
+            dbContext.Add(disciplina);
+            dbContext.SaveChanges();
+        }
+    }
+
+   public class GeradorTesteDbContext : DbContext
+    {
+        public DbSet<Disciplina> Disciplinas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=GeradorDeTestes;Integrated Security=True;Pooling=False";
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Disciplina>(disciplinaBuilder =>
+            {
+                disciplinaBuilder.ToTable("TBDisciplina");
+
+                disciplinaBuilder.Property(d => d.Id)
+                .IsRequired()
+                .ValueGeneratedOnAdd();
+
+                disciplinaBuilder.Property(d => d.Nome)
+                .IsRequired()
+                .HasColumnType("vachar(150)");
+            });
+        }
+    }
+}
